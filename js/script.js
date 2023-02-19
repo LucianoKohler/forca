e09164025543e097ad17a99a9b87f3f2403447d9
Luciano K. da Silva <lucianokohlerdasilva@gmail.com>
2023-02-19 12:15:01 -0300

    adicionado vidas e o resto dos sprites

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-const buttons = document.querySelectorAll('#kbd-btn')
-const button = document.getElementById('kbd-btn')
+var lives = 6;
+var counter = document.getElementById('lives')
+var svg = document.getElementById('svg')
+var svgvalue = 0
+var svgpath = 'img/forca-'+svgvalue+'.svg'
 
-button.addEventListener('click', function(){
-    console.log(button.dataset.type)
+if (lives==0){
+    counter.innerHTML='VOCÊ PERDEU!'
+}
+document.addEventListener('keydown', e =>{
+    lives-=1
+    svgvalue+=1
+    svgpath = 'img/forca-'+svgvalue+'.svg'
+    counter.innerHTML='VIDAS RESTANTES: ' + lives
+    svg.src=svgpath
+    console.log(svgpath)
 })
+
