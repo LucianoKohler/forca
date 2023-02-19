4f50b73d7d49c9edb05903b286de0f0f3658bf23
Luciano K. da Silva <lucianokohlerdasilva@gmail.com>
2023-02-19 13:09:41 -0300

    arrumado a ordem dos SVGs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 var lives = 6;
 var counter = document.getElementById('lives')
 var svg = document.getElementById('svg')
-var svgvalue = 0
-var svgpath = 'img/forca-'+svgvalue+'.svg'
+var svgpath = 'img/forca-'+lives+'.svg'
 
 if (lives==0){
     counter.innerHTML='VOCÊ PERDEU!'
 }
 document.addEventListener('keydown', e =>{
     lives-=1
-    svgvalue+=1
-    svgpath = 'img/forca-'+svgvalue+'.svg'
+    svgpath = 'img/forca-'+lives+'.svg'
     counter.innerHTML='VIDAS RESTANTES: ' + lives
     svg.src=svgpath
     console.log(svgpath)
