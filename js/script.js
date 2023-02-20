0e3654ed7d72e098182f842e25c268a9e9018db9
Luciano K. da Silva <lucianokohlerdasilva@gmail.com>
2023-02-20 17:26:34 -0300

    progresso nenhum :(

--- js/script.js
+++ js/script.js
@@ -3,14 +3,21 @@ var counter = document.getElementById('lives')
 var svg = document.getElementById('svg')
 var svgpath = 'img/forca-'+lives+'.svg'
 
-if (lives==0){
-    counter.innerHTML='VOCÊ PERDEU!'
-}
+var words=[
+    'ábaco','alcorão',
+    'queijo','formal',
+    'toalha','musgo',
+    'tártaro','carroceria',
+    'pingando','corrida',
+    'armado', 'pirralho',
+    'homogênio', 'hipopótamo'
+]
 document.addEventListener('keydown', e =>{
     lives-=1
     svgpath = 'img/forca-'+lives+'.svg'
     counter.innerHTML='VIDAS RESTANTES: ' + lives
     svg.src=svgpath
-    console.log(svgpath)
 })
+
+var word = words[Math.floor(Math.random() * words.length)]
 
